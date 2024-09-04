7e43e52886b311c1c5e2d1ed19073a9287c71e9f
Atiq Mansoori <er_asm@outlook.com>
2024-09-04 11:54:48 +0530

    .

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 from fastapi import FastAPI
+from transformers import pipeline
+
+
+## Create FastAPi instance
+
+app = FastAPI()
+
+# Init Text Generation pipeline
+
+pipe = pipeline("text2text-generation", model="google/flan-t5-small")
+
+@app.get('/')
+def home():
+    return {"a":"s"}
+
+@app.get("/gen")
+def generate(text:str):
+    output=pipe(text)
+    return {"output": output[0]["generated_text"]}
